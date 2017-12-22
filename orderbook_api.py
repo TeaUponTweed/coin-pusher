43315ed663f40d29b44d3cf95ed2b2f254b41fc5
Garrett Idler <garrettidler@gmail.com>
2017-12-22 15:11:39 -0700

    Fixes/Getting closer to runnable

--- orderbook_api.py
+++ orderbook_api.py
@@ -6,27 +6,34 @@ import json
 
 def pairwise(iterable):
     "s -> (s0,s1), (s1,s2), (s2, s3), ..."
-    a, b = tee(iterable) next(b, None)
+    a, b = tee(iterable)
+    next(b, None)
     return zip(a, b)
 
 class SimpleNodeVisitor():
-   def __init__(self, product_list, path=None):
+   def __init__(self, path=None):
        self.path = path or []
-       self.product_list = product_list
 
    def __iter__(self):
        yield self.path
-       for node in self.all_coins:
+       for node in all_coins:
            if node not in self.path:
-               if not self.path or is_valid_transition(self.path[-1], node, self.product_list):
+               if not self.path or is_valid_transition(self.path[-1], node, product_list):
                    yield from SimpleNodeVisitor(self.path + [node])
 
+coin_increments = {
+    'USD': 0.01,
+    'BTC': 0.0001,
+    'ETH': 0.001,
+    'LTC': 0.01
+}
 all_coins = list(coin_increments)
+product_list = ["BTC-USD","ETH-USD","LTC-USD","ETH-BTC","LTC-BTC"]
 
 def is_valid_transition(a, b, product_list):
     return '{}-{}'.format(a, b) in product_list or '{}-{}'.format(b, a) in product_list
 
-def trade_stats(base_currency, base_number, next_currency, coin_increments): # TODO: add outstanding volume at bid/ask price for time calculation
+def trade_stats(base_currency, base_number, next_currency, order_book_map): # TODO: add outstanding volume at bid/ask price for time calculation
     trade = '{}-{}'.format(base_currency, next_currency)
     if trade not in order_book_map:
         trade = '{}-{}'.format(next_currency, base_currency)
@@ -45,16 +52,18 @@ def trade_stats(base_currency, base_number, next_currency, coin_increments): # T
     return trade_arbitrage, trade_time, next_number
 
 def loop_profit(loop, base_number, order_book_map, volume_map):
-    loop = loop.append(loop[0])
+    loop.append(loop[0])
     loop_arbitrage = 1.0
     loop_time = 0.0
     intermediate_number = base_number
+    print("Loop: {}, Volume: {}".format(loop, base_number))
     for base_currency, next_currency in pairwise(loop):
-        trade_arbitrage, trade_time, trade_number = trade_stats(base_currency, intermediate_number, next_currency)
+        trade_arbitrage, trade_time, trade_number = trade_stats(base_currency, intermediate_number, next_currency, order_book_map)
         loop_arbitrage *= trade_arbitrage
         loop_time += trade_time
         intermediate_number = trade_number
-    return (loop_arbitrage - ) / loop_time
+    print("loop_arbitrage: {}, time: {}".format(loop_arbitrage, loop_time))
+    return (loop_arbitrage - 1) / loop_time
 
 def make_volume_map(order_book_map, public_client):
     volume_map = {}
@@ -67,8 +76,9 @@ def make_volume_map(order_book_map, public_client):
 def next_move(starting_with, number, order_book_map, volume_map, product_list):
     def f(path):
         return loop_profit(path, number, order_book_map, volume_map)
-    all_paths = list(SimpleNodeVisitor([starting_with], product_list))
-    profit, path = max(zip(map(f, all_paths), paths), key=lambda x: x[0])
+    all_paths = list(SimpleNodeVisitor([starting_with]))[1:] # Is there a better way to avoid the loop of lenth 1?
+    print("All paths: {}".format(all_paths))
+    profit, path = max(zip(map(f, all_paths), all_paths), key=lambda x: x[0])
     if profit > 0:
         return (path[1], profit)
     else:
@@ -92,39 +102,40 @@ def get_api_credentials(api_credential_file = 'api_credentials.json', sandbox = 
     api_key = credentials["key"]
     api_secret = credentials["b64secret"]
 
-    return api_passphrase, api_key, api_secret
+    return api_key, api_secret, api_passphrase
 
 def run():
 
     auth_client = gdax.AuthenticatedClient(*get_api_credentials())
     public_client = gdax.PublicClient()
-    // product_list = ["BTC-USD","ETH-USD","LTC-USD","ETH-BTC","LTC-BTC"]
-    product_list = public_client.get_products()
+    # products = public_client.get_products()
 
-    '''
     coin_increments = {
         'USD': 0.01,
         'BTC': 0.0001,
         'ETH': 0.001,
         'LTC': 0.01
     }
-    '''
-    coin_increments = public_client.get_product_increments() # TODO: this call doesn't exist, but I'd like to do it programmatically
+
+    # coin_increments = public_client.get_product_increments() # TODO: this call doesn't exist, but I'd like to do it programmatically
 
     order_book_map = {k:gdax.OrderBook(product_id=k) for k in product_list}
     volume_map = make_volume_map(order_book_map, public_client)
 
     while True:
-        current_coins = auth_client.get_accounts()
+        # account_dict = {account['currency']:float(account['available']) for account in auth_client.get_accounts()}
+        account_dict = {'ETH': 1.0, 'BTC': 1.0, 'USD': 2.4, 'LTC': 1.0, 'BCH': 1.0} # Test
+        print("Account: {}".format(account_dict))
 
         next_trades = []
 
-        for coin, number in current_coins:
+        for coin, number in account_dict.items():
             next_step = next_move(coin, number, order_book_map, volume_map, product_list)
             if next_step:
-                next_trades.append((coin, number, next_step[0], next_step[1])
+                next_trades.append((coin, number, next_step[0], next_step[1]))
 
-        sorted(next_trades, key=lambda x: x[3], reverse = True)
+        import ipdb; ipdb.set_trace()
+        next_trades.sort(key=lambda x: x[3], reverse = True)
 
         for entry in next_trades:
             make_trade(entry, _, number) # TODO: need to pipe price out of path evalutation so we can set trade price
